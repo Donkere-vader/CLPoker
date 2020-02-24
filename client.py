b4568f35165f37a83d80d4222201f5e656e50a6b
Donkere-vader <ctweeduizendvier@yahoo.com>
2020-02-24 12:23:26 +0100

    I can't come up with a good message for this commit

--- client.py
+++ client.py
@@ -2,3 +2,128 @@ from tkinter import Tk, Button, Entry, Frame, Label, messagebox
 import socket
 import json
 import threading
+
+PLAYING_CARDS = []
+for color in ['heart', 'tiles', 'clovers', 'pikes']:
+    for i in range(0, 14):
+        num = i
+        PLAYING_CARDS.append({
+            "color":color,
+            "value":num
+        })
+
+class Client:
+    def __init__(self, ip, port):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((ip, port))
+
+    def start(self):
+        while True:
+                    try:
+                        data = self.sock.recv(1024)
+                        if not data:
+                            continue
+                        data = json.loads(data.decode("utf-8"))
+                    except ConnectionResetError:
+                        messagebox.showerror("ERROR", "Connection to server lost...")
+                        game.main()
+                        return
+                    if data['type'] == 'tables':
+                        game.tables(data['tables'])                
+                
+class Game:
+    def __init__(self):
+        self.root = Tk()
+        self.root.title = "CLPoker"
+        self.main()
+        self.client = None
+
+    def send(self, data):
+        self.client.sock.send(bytes(json.dumps(data), "utf-8"))
+
+    def clear_window(self, window):
+        for i in window.winfo_children():
+            i.destroy()
+
+    def main(self):
+        self.clear_window(self.root)
+        Label(
+            text="CL Poker",
+            font='arial 20'
+        ).grid()
+        Label(
+            text="Connect to a poker server",
+            font='arial 12'
+        ).grid()
+        Label(
+            text="",
+            font='arial 12'
+        ).grid()
+        Label(
+            text="Server ip:",
+            anchor='nw',
+            justify="left",
+            font='arial 12'
+        ).grid(sticky='w')
+        ip_entry = Entry(
+            font='arial 20',
+            width=20
+        )
+        ip_entry.grid()
+        Label(
+            text="Server port:",
+            anchor='nw',
+            justify="left",
+            font='arial 12'
+        ).grid(sticky='w')
+        port_entry = Entry(
+            font='arial 20',
+            width=20
+        )
+        port_entry.grid()
+        Button(
+            text='Connect',
+            font='arial 20',
+            width=18,
+            command= lambda ip_entry=ip_entry, port_entry=port_entry: self.connect(ip_entry, port_entry)
+        ).grid()
+
+    def connect(self, ip_entry, port_entry):
+        ip = ip_entry.get()
+        try:
+            port = int(port_entry.get())
+        except ValueError:
+            messagebox.showerror("ERROR", "Invalid port")
+        self.cThread = threading.Thread(target=self.start_connection, args=(ip, port))
+        self.cThread.daemon = True
+        self.cThread.start()
+        self.clear_window(self.root)
+
+    def tables(self, tables):
+        Label(
+            text=f"Connected to server {self.client.sock.getsockname()[0]}:{self.client.sock.getsockname()[1]}",
+            bg='green',
+            fg="white"
+        ).grid()
+        Label(
+            text="Choose a table",
+            font='arial 20'
+        ).grid()
+        for table in tables:
+            Button(
+                text=table['name'],
+                width=20,
+                font='arial 20',
+                comman= lambda port=table['port']: self.join_table(port)
+            ).grid()
+
+    def join_table(self, port):
+        self.client = Client(self.client.sock.getsockname()[0], port)
+        self.client.start()
+
+    def start_connection(self, ip, port):
+        self.client = Client(ip, port)
+        self.client.start()
+
+game = Game()
+game.root.mainloop()
--- server.py
+++ server.py
@@ -3,8 +3,17 @@ import json
 import os
 import threading
 import datetime
+import random
 
 IP = '0.0.0.0'
+PLAYING_CARDS = []
+for color in ['heart', 'tiles', 'clovers', 'pikes']:
+    for i in range(0, 14):
+        num = i
+        PLAYING_CARDS.append({
+            "color":color,
+            "value":num
+        })
 
 def print_logo():
     print("""\u001b[31m
@@ -26,9 +35,10 @@ class Table:
         self.sock.listen(1)
         self.on_turn = 0
         self.pot = 0
-        self.stake = 10
+        self.stake = stake
         self.call_to = self.stake
 
+    def start(self):
         while True:
             c, a = self.sock.accept()
             cThread = threading.Thread(target=self.handler, args=(c, a))
@@ -40,9 +50,17 @@ class Table:
         server.log.append(f"\u001b[31m[+]\u001b[0m {a} Connected to table {self.name}")
         server.console_out()
         while True:
-            data = json.loads(self.sock.recv(1024).decode("utf-8"))
+            data = c.recv(1024)
+            if not data:
+                continue
+            data = json.loads(data.decode("utf-8"))
             if data['type'] == 'move':
-                if data['move']
+                if data['move'] == 'call':
+                    pass
+                if data['move'] == 'check':
+                    pass
+                if data['move'] == 'raise':
+                    pass
 
 class server:
     def __init__(self):
@@ -64,22 +82,45 @@ class server:
             self.connections.append(c)
 
     def handler(self, c, a):
+        tables = []
+        for table in self.tables:
+            tables.append({
+                "name":table.name,
+                "port":table.port
+            })
         data = {
             "type":"tables",
-            "tables":self.tables
+            "tables":tables
         }
+        self.connections.append(c)
         c.send(bytes(json.dumps(data), "utf-8"))
-        self.log.append("\u001b[32m[{self.get_time()}]\u001b[0m New connection with {a}")
+        self.log.append(f"\u001b[32m[{self.get_time()}]\u001b[0m New connection with {a}")
         self.console_out()
         while True:
             try:
-                data = json.loads(self.sock.recv(1024).decode("utf-8"))
+                data = c.recv(1024)
+                if not data:
+                    continue
+                data = json.loads(data.decode("utf-8"))
             except ConnectionResetError:
-                self.log.append("\u001b[31m[{self.get_time()}]\u001b[0m Lost connection with {a}")
+                self.log.append(f"\u001b[31m[{self.get_time()}]\u001b[0m Lost connection with {a}")
                 self.console_out()
+                self.connections.remove(c)
+                return
             if data['type'] == 'new_table':
-                data['name']
+                table_port = self.new_table(data['name'], data['stake'])d
+                data = {
+                    "type":"table",
+                    "port":table_port
+                }
+                c.send(bytes(json.dumps(data), "utf-8"))
     
+    def new_table(self, name, stake):
+        new_table = Table(name, stake)
+        self.tables.append(new_table)
+        new_table.start()
+        return new_table.port
+
     def get_time(self):
         date = datetime.datetime.now()
         time = f"{date.hour if date.hour > 9 else ('0' + str(date.hour))}:{date.minute if date.minute > 9 else ('0' + str(date.minute))} {date.day}/{date.month}/{date.year}"
@@ -88,12 +129,12 @@ class server:
     def console_out(self):
         os.system('cls')
         print_logo()
-        print(f"\u001b[32m>>> SERVER ONLINE <<<\nIP: {socket.gethostbyname(socket.gethostname())}\nPORT: {self.sock.getsockname()[1]}\u001b[0m\n")
-        print("\n[ TABLES ]")
+        print(f"\u001b[32m>>> SERVER ONLINE <<<\nIP: {socket.gethostbyname(socket.gethostname())}\nPORT: {self.sock.getsockname()[1]}\u001b[0m")
+        print("\n[[ === TABLES === ]]")
         for table in self.tables:
             tabs = 2
             tabs -= len(table.name) // 8
-            print(table.name + ("\t" * tabs), end="")
+            print(table.name + ("\t" * tabs) + str(table.port) + ("\t" * tabs), end="")
             if len(table.connections) > 5:
                 print(f"\u001b[31m{table.connections}/6\u001b[0m")
             else:
